7a628a642ba5a26338174c7e226dc3c94d1feaf5
Omer Sabah <omerrj99@gmail.com>
2021-06-02 23:36:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+const superbowlWin = (record) => {
+  let result = record.find( record => record.result === "W" )
+  return !!result ? result.year : undefined
+}
